4a22630aa13369828496557f698061e83f30907a
williamj <williamj949@gmail.com>
2023-09-14 07:44:07 -0700

    Update ImagesFragment layout

--- app/src/main/java/com/sublime/redpix/view/ImagesFragment.kt
+++ app/src/main/java/com/sublime/redpix/view/ImagesFragment.kt
@@ -2,9 +2,13 @@ package com.sublime.redpix.view
 
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
+import com.sublime.redpix.R
 import com.sublime.redpix.databinding.FragmentImagesBinding
 
 class ImagesFragment: Fragment() {
@@ -15,6 +19,35 @@ class ImagesFragment: Fragment() {
         savedInstanceState: Bundle?
     ): View {
         val binding = FragmentImagesBinding.inflate(inflater,container, false)
+
+        setHasOptionsMenu(true)
         return binding.root
+    }
+
+    @Deprecated("Deprecated in Java")
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu_images, menu)
+        setSearchView(menu)
+    }
+
+    /**
+     * SearchView configuration
+     */
+    private fun setSearchView(menu: Menu) {
+        val searchItem = menu.findItem(R.id.action_search)
+
+        (searchItem.actionView as SearchView).apply {
+            queryHint = getString(R.string.subreddit)
+
+            setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+                override fun onQueryTextSubmit(query: String?): Boolean {
+                    return true
+                }
+
+                override fun onQueryTextChange(newText: String?): Boolean {
+                    return true
+                }
+            })
+        }
     }
 }
